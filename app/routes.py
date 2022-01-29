112230b870d9da821ee08ffde028a055e2d68eb1
bkrajews <01167581@pw.edu.pl>
2022-01-30 00:14:28 +0100

    Latest

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
 from base64 import decode
+from threading import currentThread
 from flask import Flask, render_template,url_for,request,redirect,Response,session,send_file,flash
 from app.forms import RegistrationForm,LoginForm,UpdateAccountForm
 from flask_sqlalchemy import SQLAlchemy
@@ -22,9 +23,22 @@ def index():
 def about():
     return render_template('error.html', title = "Ekran")
 
-@app.route("/products")
+@app.route("/products", methods=['POST', 'GET'])
 def products():
-    return render_template('products.html',title = "Products")
+    if request.method =="POST":
+        if request.form.get('Haczyki') == 'Haczyki':
+            products = Product.query.filter(Product.type_id == 1).all()
+            return render_template('products.html',title = "Products",products = products)
+        if request.form.get('Wedki') == 'Wedki':
+            products = Product.query.filter(Product.type_id == 2 ).all()
+            return render_template('products.html',title = "Products",products = products)
+
+        if request.form.get('Reszta') == 'Reszta':
+            products = Product.query.filter(Product.type_id == 3 ).all()
+            return render_template('products.html',title = "Products",products = products)
+    if request.method =="GET":
+        products = Product.query.order_by(Product.id).all()
+        return render_template('products.html',title = "Products",products = products)
 
 @app.route("/register",methods = ['GET','POST'])
 def register():
@@ -33,8 +47,11 @@ def register():
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_password=bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        cart = Cart()
+        db.session.add(cart)
+        db.session.commit()
         user = User(username=form.username.data
-       ,email = form.email.data, password = hashed_password)
+       ,email = form.email.data, password = hashed_password,cart_id = cart.id)
         db.session.add(user)
         db.session.commit()
         flash(f'Your account have been created, you can log in!','success')
@@ -65,7 +82,9 @@ def logout():
 @app.route("/cart",methods=['GET', 'POST'])
 @login_required
 def cart():
-    return render_template('cart.html',title = "Cart")
+    id = current_user.cart_id
+    cart = Cart.query.filter(Cart.id == id).all()
+    return render_template('cart.html',title = "Cart", cart=cart)
 
 @app.route("/account",methods=['GET', 'POST'])
 @login_required
@@ -89,3 +108,35 @@ def account():
         
     return render_template('account.html',title ="Account",form = form)
 
+
+@app.route("/delete",methods=['GET', 'POST'])
+@login_required
+def delete():
+    if request.method =="POST":
+        if request.form.get('Delete') == 'Delete':
+            user = User.query.filter_by(username =current_user.username).delete()
+            db.session.commit()
+            flash('Your account has been deleted','danger')
+            return redirect(url_for('logout'))
+    elif request.method =="GET":
+        return render_template("delete.html")
+
+
+@app.route("/add_to_cart",methods =['GET','POST'])
+@login_required
+def add_to_cart():
+    if request.method =="POST":
+        if request.form.get("Add to cart") == "Add to cart":
+            product_id = request.form.get("hidden")
+            cart_id = current_user.cart_id
+            print(cart_id)
+            product_update = Product.query.filter(Product.id == product_id).all()
+            query_cart = Cart.query.filter(Cart.id == cart_id).all()
+            product_update[0].has_cart.append(query_cart[0])
+            db.session.commit()
+            flash(f'Product has been added to cart!','success')
+    return redirect(url_for('products')) 
+
+@app.route("/like",methods =['GET','POST'])
+def like():
+    return render_template("cart.html")

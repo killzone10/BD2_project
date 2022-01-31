f5ef05e9cf54b0f6a4fba2549ca8c4aee06d1b1b
bkrajews <01167581@pw.edu.pl>
2022-01-31 16:31:19 +0100

    heroku commit

--- app/__init__.py
+++ app/__init__.py
@@ -5,12 +5,12 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 import os 
-# p = os.environ.get('SECREET_KEY')
+p = os.environ.get('SECRET_KEY')
 app = Flask(__name__, template_folder='./templates')
 
 # app.secret_key = '\xea\x1a\xb2\x8a\xefk\xd6V%\xf7\xb4\xe5\xa9\r=&'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:linux123@localhost/products' ## postgres ://nazwa uzytkownima : haslo @ localhost/nazwa bazy danych
-app.config['SECRET_KEY']='f027a3f2e1b601db1ae08006930c074f'
+app.config['SECRET_KEY']=p
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
